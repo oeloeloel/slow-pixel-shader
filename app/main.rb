7c8013ccbbf9cd47a008b139e96bf1d833c70920
oeloeloel <leo.prendergast@gmail.com>
2021-07-15 15:58:30 -0400

    .

--- app/main.rb
+++ app/main.rb
@@ -1,5 +1,5 @@
 # slower version
 require 'app/shader-module.rb'
-require 'app/shader-target.rb'
-# require 'app/bwspiral.rb'
+# require 'app/shader-target.rb'
+require 'app/bwspiral.rb'
 require 'app/tick.rb'
